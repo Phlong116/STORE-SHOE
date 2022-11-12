3aa3a8df1ae5f56c39ee36900bf99a252ed7f919
hoanglong116 <Phlong116@gmail.com>
2022-11-12 22:14:08 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Cart from "./EX1/Cart";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Cart />
+      </div>
+    );
+  }
+}
+
+export default App;
